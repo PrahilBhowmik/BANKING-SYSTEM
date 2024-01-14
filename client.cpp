8347403af1e4bebfd5b31f81e4a33561e955c3ee
PrahilBhowmik <mebhowmik321@gmail.com>
2024-01-14 18:35:50 +0530

    Implement Login

--- client.cpp
+++ client.cpp
@@ -1,101 +1,10 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-char clientType;
-
-pair<bool,string> login(string userId, string password){
-    // Request to Server
-    clientType = 'C';
-    return make_pair(true,"LOGGED IN SUCCESSFULLY\n");
-}
-
-void printMiniStatement(string userId){
-    // Request to Server
-    cout<<"PRINTING MINI STATEMENT"<<endl;
-}
-
-void printBalance(string userId){
-    // Request to Server
-    cout<<"PRINTING YOUR BALANCE"<<endl;
-}
-
-void printAllBalances(){
-    // Request to Server
-    cout<<"PRINTING ALL BALANCES"<<endl;
-}
-
-void credit_balance(){
-    string userId;
-    double amount;
-    cout<<"Enter userId: ";
-    cin>>userId;
-    cout<<"Enter amount to credit: ";
-    cin>>amount;
-    // Request to Server
-    cout<<"CREDITING AMOUNT"<<endl;
-}
-
-void debit_balance(){
-    string userId;
-    double amount;
-    cout<<"Enter userId: ";
-    cin>>userId;
-    cout<<"Enter amount to debit: ";
-    cin>>amount;
-    // Request to Server
-    cout<<"DEBITING AMOUNT"<<endl;
-}
-
-void handleAdmin(){
-    while(true){
-        cout<<"Please choose an action:\n1.Credit Balance\n2.Debit Balance\n3.Exit\nYour Choice: ";
-        int choice;
-        cin>>choice;
-        system("clear");
-        switch(choice){
-            case 1: credit_balance();break;
-            case 2: debit_balance();break;
-            case 3: return;
-            default: cout<<"INVALID CHOICE!!"<<endl;break;
-        }
-    }
-}
-
-void handleCustomer(string userId){
-    while(true){
-        cout<<"Please choose an action:\n1.See Available Balance\n2.Get mini statement\n3.Exit\nYour Choice: ";
-        int choice;
-        cin>>choice;
-        system("clear");
-        switch(choice){
-            case 1: printBalance(userId);break;
-            case 2: printMiniStatement(userId);break;
-            case 3: return;
-            default: cout<<"INVALID CHOICE!!"<<endl;break;
-        }
-    }
-}
-
-void handlePolice(){
-    while(true){
-        cout<<"Please choose an action:\n1.See Available Balances of all customers\n2.Get mini statement\n3.Exit\nYour Choice: ";
-        int choice;
-        string userId;
-        cin>>choice;
-        system("clear");
-        switch(choice){
-            case 1: printAllBalances();break;
-            case 2: cout<<"Enter userId: ";
-                    cin>>userId;
-                    printMiniStatement(userId);break;
-            case 3: return;
-            default: cout<<"INVALID CHOICE!!"<<endl;break;
-        }
-    }
-}
-
+#include "clientUtils.h"
+ 
 int main(){
-    clientType = 'N';
+    if(!connect()){
+        return 0;
+    }
+    clientType = 'C';
     bool loggedIn=false;
     string userId;
     while(!loggedIn){
@@ -118,5 +27,6 @@ int main(){
     }
     system("clear");
     cout<<"LOGGING OUT"<<endl;
+    close(client_fd);  
     return 0;
 }
---
+++ clientUtils.h
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+#include <arpa/inet.h>
+#include <sys/socket.h> 
+using namespace std;
+
+#define PORT 8080 
+
+char clientType;
+int sock = 0, valread, client_fd;  
+struct sockaddr_in serv_addr; 
+
+char buffer[1024] = { 0 }; 
+
+bool connect(){ 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {  
+            printf("\n Socket creation error \n");  
+            return false;  
+    }  
+
+    serv_addr.sin_family = AF_INET;  
+    serv_addr.sin_port = htons(PORT);  
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)  
+            <= 0) {  
+            printf(  
+                    "\nInvalid address/ Address not supported \n");  
+            return false;  
+    }  
+
+    if ((client_fd  
+            = connect(sock, (struct sockaddr*)&serv_addr,  
+                            sizeof(serv_addr)))  
+            < 0) {  
+            printf("\nConnection Failed \n");  
+            return false;  
+    }
+    return true;  
+}
+
+pair<bool,string> login(string userId, string password){
+    // Request to Server
+    string s = "login " + userId + " " + password;
+    const int length = s.length(); 
+    char* request = new char[length + 1];  
+    strcpy(request, s.c_str());
+    send(sock, request, strlen(request), 0);  
+    printf("Hello message sent\n");  
+    valread = read(sock, buffer, 1024);  
+    s=buffer;
+    if(s=="success"){
+        return make_pair(true,"success");
+    }
+    return make_pair(false,"failure");
+}
+
+void printMiniStatement(string userId){
+    // Request to Server
+    cout<<"PRINTING MINI STATEMENT"<<endl;
+}
+
+void printBalance(string userId){
+    // Request to Server
+    cout<<"PRINTING YOUR BALANCE"<<endl;
+}
+
+void printAllBalances(){
+    // Request to Server
+    cout<<"PRINTING ALL BALANCES"<<endl;
+}
+
+void credit_balance(){
+    string userId;
+    double amount;
+    cout<<"Enter userId: ";
+    cin>>userId;
+    cout<<"Enter amount to credit: ";
+    cin>>amount;
+    // Request to Server
+    cout<<"CREDITING AMOUNT"<<endl;
+}
+
+void debit_balance(){
+    string userId;
+    double amount;
+    cout<<"Enter userId: ";
+    cin>>userId;
+    cout<<"Enter amount to debit: ";
+    cin>>amount;
+    // Request to Server
+    cout<<"DEBITING AMOUNT"<<endl;
+}
+
+void handleAdmin(){
+    while(true){
+        cout<<"Please choose an action:\n1.Credit Balance\n2.Debit Balance\n3.Exit\nYour Choice: ";
+        int choice;
+        cin>>choice;
+        system("clear");
+        switch(choice){
+            case 1: credit_balance();break;
+            case 2: debit_balance();break;
+            case 3: return;
+            default: cout<<"INVALID CHOICE!!"<<endl;break;
+        }
+    }
+}
+
+void handleCustomer(string userId){
+    while(true){
+        cout<<"Please choose an action:\n1.See Available Balance\n2.Get mini statement\n3.Exit\nYour Choice: ";
+        int choice;
+        cin>>choice;
+        system("clear");
+        switch(choice){
+            case 1: printBalance(userId);break;
+            case 2: printMiniStatement(userId);break;
+            case 3: return;
+            default: cout<<"INVALID CHOICE!!"<<endl;break;
+        }
+    }
+}
+
+void handlePolice(){
+    while(true){
+        cout<<"Please choose an action:\n1.See Available Balances of all customers\n2.Get mini statement\n3.Exit\nYour Choice: ";
+        int choice;
+        string userId;
+        cin>>choice;
+        system("clear");
+        switch(choice){
+            case 1: printAllBalances();break;
+            case 2: cout<<"Enter userId: ";
+                    cin>>userId;
+                    printMiniStatement(userId);break;
+            case 3: return;
+            default: cout<<"INVALID CHOICE!!"<<endl;break;
+        }
+    }
+}
